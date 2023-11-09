ca3eba9c7489eac81ebfe9721d6441b873478ecf
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2023-11-09 08:13:10 +0600

    Initial Commit

---
+++ SpaceProj/SpaceProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceProjApp.swift
+//  SpaceProj
+//
+//  Created by Tanvir on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
